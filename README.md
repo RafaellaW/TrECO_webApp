8fcd8ca884127e92c1856ef4a1b33122edafe8d2
RafaellaW <2106227@aluno.univesp.br>
2023-05-22 22:17:32 -0300

    Atualizações de bairros na home.py, readme e database(novos testestes)

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# TrECO_webApp
+# TrECO – Recicle, reduza e reuse: aplicação web para gerenciamento de resíduos recicláveis domésticos
+
+UNIVESP - BTI
+Projeto Integrador
+Grupo 001
+
+Autores:
+
+Caio Alexandre de Oliveira Ferreira,   
+Dénnyel Victor Silva de Oliveira, 
+Jonas Rudolf Carregari,
+Micaela Viana Rodrigues,
+Misleine Pereira Silva Celes, 
+Murilo José Fernandes, 
+Rafaella Sttefany Guimarães Wiziack, 
+Thiago Andre Gomes Nishida, 
--- home.py
+++ home.py
@@ -61,7 +61,20 @@ if acao == "Login":
 
 elif acao == "Cadastro":
     nomeCompleto = st.sidebar.text_input('Nome Completo')
-    bairro = st.sidebar.selectbox(label="Bairro", options=["Aeroporto", "Alpa", "Alto Sumaré", "América"])
+    bairro = st.sidebar.selectbox(label="Bairro", options=["Aeroporto", "Alpa", "Alto Sumaré", "América", "Área Rural de Barretos", 
+	"Área Rural de Ibitu", "Baroni", "Bela Vista", "Benedito Realindo Corrêa",	"Boa Esperança", "Boa Vista", "Bom Jesus", "Campos do Conde", "Cecapinha", "Celina", "Centro", "Chacara dos Ipes", "Chacaras Feitosa"
+	"Cidade Jardim", "City Barretos", "Clementina", "Conjunto Residencial Batista Ananias", "Conjunto Habitacional Helly Pimenta", 
+	"Conjunto Habitacional José Eugênio Abrão Miziara - Jone Miziara", "Conjunto Habitacional Leda Silveira Amendola", "Conjunto Habitacional Michel Jorge Naben",
+	"Conjunto Habitacional Newton Siqueira Sopa", "Conjunto Habitacional Rubens de Souza", "Conjunto Habitacional Vida Nova", "Cristiano de Carvalho", "Derby CLub",
+	"Distrito Industrial I", "Diva", "Dom João Bosco", "Doutor Paulo Prata", "Estância Casa Real", "Exposição", "Fortaleza", "Frigorífico", "Gomes", "Gonçalves", "Haddad",
+	"Henriqueta", "Hussein Gemha", "Ibirapuera", "Ibitu", "Industrial", "Jardim Allah", "Jardim Alvorada", "Jardim Anastácio", "Jardim Arizona", "Jardim Botânico", "Jardim Caiçara", "Jardim Califórnia",
+	"Jardim Campo Redondo", "Jardim dos Coqueiros", "Jardim Estância", "Jardim Europa", "Jardim Luis Spina", "Jardim Maria Caputi", "Jardim Nova Barretos", "Jardim Nova Barretos II",
+	"Jardim Planalto", "Jardim Ramos", "Jardim Residencial ETEMP", "Jardim Silvia", "Jardim Soares", "Jardim Universitario", "Jardim Vale do Verde", "Joaquim Pereira Mococa", "Jockey Club",
+	"Licia", "Los Angeles", "Marchi", "Marieta", "Marilia", "Monte Alegre", "Monte Castelo", "Nadir Kenan", "Nobre Vile", "Nogueira", "Nova America", "Nova Barretos III", "Nova Clementina",
+	"Oasis", "Oriente", "Ortega", "Parque do Jóquei", "Parque Residencia Minerva","Pimenta" "Pedro Cavalini", "Pereira", "Pimenta", "Primavera", "Residencial Grande Horizonte", "Residencial Ide Daher",
+	"Residencial Jockey Club", "Residencial Mais Parque", "Residencial Parque das Flores", "Residencial Prata", "Residencial Santa Rita", "Rios", "Santa Cecilia", "Santa Helena", "Santa Izabel",
+	"Santa Terezinha", "Santana", "San Diego", "São Francisco", "São Jorge", "São Judas Tadeu", "São Salvador", "São Vicente", "Tamboré", "Zequinha Amêndola", "Zona de Uso Diversificado Pedro Pinto Paixão"
+])
     login_usuario = st.sidebar.text_input('Usuário')
     senha_usuario = st.sidebar.empty()
     senha_usuario = senha_usuario.text_input('Senha', type='password')
